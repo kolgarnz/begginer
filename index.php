072d1be9cdbbc21bbce6e985b25b90db1e6a18a3
Igor Pinchuk <i.pinchuk@qsoft.ru>
2015-05-20 11:40:18 +0300

    57037 / Создать структуру сайта / Небольшие тестовые праквки index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $APPLICATION->SetTitle("Главная");
 	Array(
 		"SEF_MODE" => "N",
 		"IBLOCK_TYPE" => "news",
-		"IBLOCK_ID" => "3",
+		"IBLOCK_ID" => "24",
 		"NEWS_COUNT" => "20",
 		"SORT_BY1" => "ACTIVE_FROM",
 		"SORT_ORDER1" => "DESC",
@@ -17,11 +17,97 @@ $APPLICATION->SetTitle("Главная");
 		"ADD_SECTIONS_CHAIN" => "Y",
 		"CACHE_TYPE" => "A",
 		"CACHE_TIME" => "3600",
-		"CACHE_NOTES" => "",
 		"VARIABLE_ALIASES" => Array(
 			"SECTION_ID" => "SECTION_ID",
 			"ELEMENT_ID" => "ELEMENT_ID"
 		)
+	)
+);?>
+<div>
+  <br />
+</div>
+
+<div>Новинки</div>
+
+<div>
+  <br />
+</div>
+
+<div><?$APPLICATION->IncludeComponent(
+	"bitrix:catalog",
+	"",
+	Array(
+		"AJAX_MODE" => "N",
+		"SEF_MODE" => "N",
+		"IBLOCK_TYPE" => "catalog",
+		"IBLOCK_ID" => "25",
+		"USE_FILTER" => "N",
+		"USE_REVIEW" => "N",
+		"USE_COMPARE" => "N",
+		"SHOW_TOP_ELEMENTS" => "Y",
+		"SECTION_COUNT_ELEMENTS" => "Y",
+		"SECTION_TOP_DEPTH" => "2",
+		"PAGE_ELEMENT_COUNT" => "30",
+		"LINE_ELEMENT_COUNT" => "3",
+		"ELEMENT_SORT_FIELD" => "sort",
+		"ELEMENT_SORT_ORDER" => "asc",
+		"LIST_PROPERTY_CODE" => array(),
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"LIST_META_KEYWORDS" => "-",
+		"LIST_META_DESCRIPTION" => "-",
+		"LIST_BROWSER_TITLE" => "-",
+		"DETAIL_PROPERTY_CODE" => array(),
+		"DETAIL_META_KEYWORDS" => "-",
+		"DETAIL_META_DESCRIPTION" => "-",
+		"DETAIL_BROWSER_TITLE" => "-",
+		"BASKET_URL" => "/personal/basket.php",
+		"ACTION_VARIABLE" => "action",
+		"PRODUCT_ID_VARIABLE" => "id",
+		"SECTION_ID_VARIABLE" => "SECTION_ID",
+		"PRODUCT_QUANTITY_VARIABLE" => "quantity",
+		"PRODUCT_PROPS_VARIABLE" => "prop",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"SET_TITLE" => "Y",
+		"SET_STATUS_404" => "N",
+		"PRICE_CODE" => array("BASE"),
+		"USE_PRICE_COUNT" => "N",
+		"SHOW_PRICE_COUNT" => "1",
+		"PRICE_VAT_INCLUDE" => "Y",
+		"PRICE_VAT_SHOW_VALUE" => "N",
+		"PRODUCT_PROPERTIES" => array(),
+		"USE_PRODUCT_QUANTITY" => "N",
+		"LINK_IBLOCK_TYPE" => "",
+		"LINK_IBLOCK_ID" => "",
+		"LINK_PROPERTY_SID" => "",
+		"LINK_ELEMENTS_URL" => "link.php?PARENT_ELEMENT_ID=#ELEMENT_ID#",
+		"USE_ALSO_BUY" => "N",
+		"USE_STORE" => "N",
+		"USE_ELEMENT_COUNTER" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Товары",
+		"PAGER_SHOW_ALWAYS" => "Y",
+		"PAGER_TEMPLATE" => "",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "Y",
+		"TOP_ELEMENT_COUNT" => "9",
+		"TOP_LINE_ELEMENT_COUNT" => "3",
+		"TOP_ELEMENT_SORT_FIELD" => "sort",
+		"TOP_ELEMENT_SORT_ORDER" => "asc",
+		"TOP_PROPERTY_CODE" => array("STATUS"),
+		"CONVERT_CURRENCY" => "N",
+		"VARIABLE_ALIASES" => Array(
+			"SECTION_ID" => "SECTION_ID",
+			"ELEMENT_ID" => "ELEMENT_ID"
+		),
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N"
 	),
 false
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+);?></div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
