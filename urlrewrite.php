8dfd5ce4cdfe637758f05b160b1d2d35488dcceb
Igor Pinchuk <i.pinchuk@qsoft.ru>
2015-05-20 16:31:07 +0300

    57035 / Настроить стандартные группы пользователей / удалил не нужные обработчики адрессов

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,58 +1,16 @@
 <?
 $arUrlRewrite = array(
 	array(
-		"CONDITION"	=>	"#^/company/department/#",
-		"RULE"	=>	"",
-		"ID"	=>	"",
-		"PATH"	=>	"/company/department.php",
-	),
-	array(
-		"CONDITION"	=>	"#^/company/finances/#",
-		"RULE"	=>	"",
-		"ID"	=>	"",
-		"PATH"	=>	"/company/finances.php",
-	),
-	array(
-		"CONDITION"	=>	"#^/company/contacts/#",
-		"RULE"	=>	"",
-		"ID"	=>	"bitrix:main.feedback",
-		"PATH"	=>	"/company/contacts.php",
-	),
-	array(
 		"CONDITION"	=>	"#^/personal/profile/#",
 		"RULE"	=>	"",
 		"ID"	=>	"bitrix:main.profile",
 		"PATH"	=>	"/personal/profile.php",
 	),
 	array(
-		"CONDITION"	=>	"#^/company/clients/#",
-		"RULE"	=>	"",
-		"ID"	=>	"",
-		"PATH"	=>	"/company/clients.php",
-	),
-	array(
-		"CONDITION"	=>	"#^/company/stores/#",
-		"RULE"	=>	"",
-		"ID"	=>	"qsoft:stores.list",
-		"PATH"	=>	"/company/stores.php",
-	),
-	array(
-		"CONDITION"	=>	"#^/company/about/#",
-		"RULE"	=>	"",
-		"ID"	=>	"",
-		"PATH"	=>	"/company/about.php",
-	),
-	array(
 		"CONDITION"	=>	"#^/personal/cart/#",
 		"RULE"	=>	"",
 		"ID"	=>	"",
 		"PATH"	=>	"/personal/cart.php",
-	),
-	array(
-		"CONDITION"	=>	"#^/company/news/#",
-		"RULE"	=>	"",
-		"ID"	=>	"bitrix:news",
-		"PATH"	=>	"/company/news.php",
 	),
 	array(
 		"CONDITION"	=>	"#auth.php#",
