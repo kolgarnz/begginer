cb9ba790e0d17640219e0cb2bbe94c1114b06544
Igor Pinchuk <i.pinchuk@qsoft.ru>
2015-05-20 10:20:31 +0300

    57037 / Создать структуру сайта / Удалены лишние папки

--- .access.php
+++ .access.php
@@ -1,6 +1,3 @@
 <?
-$PERM["e-store"]["6"]="W";
-$PERM["e-store"]["8"]="W";
-$PERM["desktop_app"]["*"]="R";
 $PERM["/"]["*"]="R";
 ?>
--- .top.menu.php
+++ .top.menu.php
@@ -41,6 +41,13 @@ $aMenuLinks = Array(
 		Array(), 
 		Array(), 
 		"" 
+	),
+	Array(
+		"Информация", 
+		"/company/", 
+		Array(), 
+		Array(), 
+		"" 
 	)
 );
 ?>
