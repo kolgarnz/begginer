5d1d6b7e642fac34d08d17d3625d5303103f7190
Igor Pinchuk <i.pinchuk@qsoft.ru>
2015-05-22 15:03:38 +0300

    57044 / Разработка блока "Наши салоны" в футере / Исправлено название в списке компонентов

--- bitrix/components/qsoft/stores.list/lang/en/.description.php
+++ bitrix/components/qsoft/stores.list/lang/en/.description.php
@@ -1,5 +1,5 @@
 <?
 $MESS ['T_IBLOCK_DESC_STORE_LIST'] = "Random elements";
 $MESS ['T_IBLOCK_DESC_STORE_DESC'] = "Show random elements";
-$MESS ['T_IBLOCK_DESC_STORE'] = "QSOFT Extension";
+$MESS ['T_IBLOCK_DESC_STORE'] = "Stores Extension";
 ?>
