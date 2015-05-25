a7db3278aa235255210d8a020d1e71396cfd94b4
Igor Pinchuk <i.pinchuk@qsoft.ru>
2015-05-25 10:21:02 +0300

    57047 / Разработка компонента авторизация / Временно отключен из roga_i_sila_inner часть кода связанная с table

--- auth/index.php
+++ auth/index.php
@@ -2,4 +2,21 @@
 define("NEED_AUTH", true);
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 ?>
+<?if($_GET['register'] == 'yes'):?>
+	<h2>Добро пожаловать!</h2>
+	<p>Пожалуйста, проверьте Ваш e-mail – мы отправили Вам приветственное письмо.</p> 
+	<p>
+		Теперь у Вас есть возможность:
+		<ul>
+			<li>Сохранять в Личном кабинете персональные данные</li>
+			<li>Легко отслеживать статус Вашего заказа в режиме online</li>
+			<li>В любой момент просмотреть историю Ваших заказов</li>
+		</ul>
+		Что Вы хотите сделать прямо сейчас?
+	</p>
+<?elseif(strlen($_GET['backurl']) > 0):?>
+	<?LocalRedirect($_GET['backurl'])?>
+<?else:?>
+<?LocalRedirect("/")?>
+<?endif?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
