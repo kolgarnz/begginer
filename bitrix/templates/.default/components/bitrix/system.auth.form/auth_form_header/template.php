952bc21f91a5d600921947dbcae5c537034bd244
Igor Pinchuk <i.pinchuk@qsoft.ru>
2015-05-25 11:16:29 +0300

    57047 / Разработка компонента авторизация / Поправлен шаблон system.auth.authorize. Добавлен backurl

--- bitrix/templates/.default/components/bitrix/system.auth.form/auth_form_header/template.php
+++ bitrix/templates/.default/components/bitrix/system.auth.form/auth_form_header/template.php
@@ -1,12 +1,10 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
-
-
 <?if($arResult["FORM_TYPE"] == "login"):?>
 
 <nav class="top_menu grey inline-block">
 	<a href="<?=$arResult["AUTH_REGISTER_URL"]?>" class="register"><?=GetMessage("AUTH_REGISTER")?></a>
-	<a href="/auth/" class="auth"><?=GetMessage("AUTH_AUTHORIZATION")?></a>
+	<a href="/auth/<?if(strlen($arResult['BACKURL']) > 0):?>?backurl=<?=urlencode($arResult['BACKURL'])?><?endif?>" class="auth"><?=GetMessage("AUTH_AUTHORIZATION")?></a>
 </nav>
 
 <?
@@ -16,7 +14,7 @@ else:
 
 <nav class="top_menu grey inline-block authorize">
 	<span><?=GetMessage("AUTH_HELLO")?>,</span>
-	<a href="/personal/profile/"><b class="user_name"><?=$arResult["USER_NAME"]?></b></a>
+	<a href="<?=$arParams["PROFILE_URL"]?>profile/"><b class="user_name"><?=$arResult["USER_NAME"]?></b></a>
 	<a href="<?=$arParams["PROFILE_URL"]?>"><?=GetMessage("AUTH_PERSONAL_AREA")?></a>
 	<a class="logout" href="<?echo $APPLICATION->GetCurPageParam("logout=yes", array(
      "login",
