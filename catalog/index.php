2a23068875e48d4f723fa425c54ba4941faadde1
Igor Pinchuk <i.pinchuk@qsoft.ru>
2015-05-25 17:07:54 +0300

    57052 / Модель (итоговый тикет) / Сдача Модели

--- catalog/index.php
+++ catalog/index.php
@@ -1,79 +1,258 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Каталог");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:catalog",
-	"",
-	Array(
-		"AJAX_MODE" => "N",
-		"SEF_MODE" => "N",
-		"IBLOCK_TYPE" => "catalog",
-		"IBLOCK_ID" => "25",
-		"USE_FILTER" => "N",
-		"USE_REVIEW" => "N",
-		"USE_COMPARE" => "N",
-		"SHOW_TOP_ELEMENTS" => "Y",
-		"SECTION_COUNT_ELEMENTS" => "Y",
-		"SECTION_TOP_DEPTH" => "2",
-		"PAGE_ELEMENT_COUNT" => "30",
-		"LINE_ELEMENT_COUNT" => "3",
-		"ELEMENT_SORT_FIELD" => "sort",
-		"ELEMENT_SORT_ORDER" => "asc",
-		"LIST_PROPERTY_CODE" => array(),
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"LIST_META_KEYWORDS" => "-",
-		"LIST_META_DESCRIPTION" => "-",
-		"LIST_BROWSER_TITLE" => "-",
-		"DETAIL_PROPERTY_CODE" => array(),
-		"DETAIL_META_KEYWORDS" => "-",
-		"DETAIL_META_DESCRIPTION" => "-",
-		"DETAIL_BROWSER_TITLE" => "-",
-		"BASKET_URL" => "/personal/cart/",
-		"ACTION_VARIABLE" => "action",
-		"PRODUCT_ID_VARIABLE" => "id",
-		"SECTION_ID_VARIABLE" => "SECTION_ID",
-		"PRODUCT_QUANTITY_VARIABLE" => "quantity",
-		"PRODUCT_PROPS_VARIABLE" => "prop",
-		"CACHE_TYPE" => "A",
-		"CACHE_TIME" => "36000000",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"SET_TITLE" => "Y",
-		"SET_STATUS_404" => "N",
-		"PRICE_CODE" => array("BASE"),
-		"USE_PRICE_COUNT" => "N",
-		"SHOW_PRICE_COUNT" => "1",
-		"PRICE_VAT_INCLUDE" => "Y",
-		"PRICE_VAT_SHOW_VALUE" => "N",
-		"PRODUCT_PROPERTIES" => array(),
-		"USE_PRODUCT_QUANTITY" => "N",
-		"LINK_IBLOCK_TYPE" => "",
-		"LINK_IBLOCK_ID" => "",
-		"LINK_PROPERTY_SID" => "",
-		"LINK_ELEMENTS_URL" => "link.php?PARENT_ELEMENT_ID=#ELEMENT_ID#",
-		"USE_ALSO_BUY" => "N",
-		"USE_STORE" => "N",
-		"USE_ELEMENT_COUNTER" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"PAGER_TITLE" => "Товары",
-		"PAGER_SHOW_ALWAYS" => "Y",
-		"PAGER_TEMPLATE" => "",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "Y",
-		"TOP_ELEMENT_COUNT" => "9",
-		"TOP_LINE_ELEMENT_COUNT" => "3",
-		"TOP_ELEMENT_SORT_FIELD" => "sort",
-		"TOP_ELEMENT_SORT_ORDER" => "asc",
-		"TOP_PROPERTY_CODE" => array(),
-		"CONVERT_CURRENCY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"AJAX_OPTION_HISTORY" => "N",
-		"VARIABLE_ALIASES" => Array(
-			"SECTION_ID" => "SECTION_ID",
-			"ELEMENT_ID" => "ELEMENT_ID"
-		)
-	)
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><div class="filter pie">
+	<div class="b-trans-type">
+		<h3>Коробка:</h3>
+		<div class="b-trans-type__wrapper">
+			<input id="checkbox1" class="filter-type" type="checkbox">
+			<label class="filter-type-label" for="checkbox1">АКПП</label>
+		</div>
+		<div class="b-trans-type__wrapper">
+			<input id="checkbox2" class="filter-type" type="checkbox">
+			<label class="filter-type-label" for="checkbox2">МКПП</label>
+		</div>
+	</div>
+	<div class="b-slider">
+		<h3>Цена:</h3>
+		<div>
+			<input type="text" id="price-start" placeholder="от 0 руб." class="pie"/>
+			<input type="text" id="price-end" placeholder="до 10 000 000 руб." class="pie"/>
+		</div>
+		<div id="slider-range"></div>
+	</div>
+	<div class="b-color">
+		<h3>Цвет:</h3>
+		<div class="b-color__wrapper">
+			<input id="color1" class="filter-type" type="checkbox">
+			<label class="filter-type-label" for="color1">Красный</label>
+		</div>
+		<div class="b-color__wrapper">
+			<input id="color2" class="filter-type" type="checkbox">
+			<label class="filter-type-label" for="color2">Зелёный</label>
+		</div>
+		<div class="b-color__wrapper">
+			<input id="color3" class="filter-type" type="checkbox">
+			<label class="filter-type-label" for="color3">Зелёный</label>
+		</div>
+		<div class="b-color__wrapper">
+			<input id="color4" class="filter-type" type="checkbox">
+			<label class="filter-type-label" for="color4">Зелёный</label>
+		</div>
+	</div>
+					</div>
+					<section>
+	<div id="catalog">
+		<figure class="product_item">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_1.png" alt="BMW X3 2.0d" title="BMW X3 2.0d"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">BMW X3 2.0d</a></h3>
+				<p class="product_item_price dark_grey">2 230 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+		<figure class="product_item">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_2.png" alt="AUDI A6 3.0 TFSI" title="AUDI A6 3.0 TFSI"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">AUDI A6 3.0 TFSI</a></h3>
+				<p class="product_item_price dark_grey">2 870 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+
+		<figure class="product_item">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_3.png" alt="Mercedes-Benz A200" title="Mercedes-Benz A200"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">Mercedes-Benz A200</a></h3>
+				<p class="product_item_price dark_grey">1 310 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+		<figure class="product_item">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_4.png" alt="BMW Z4 sDrive35i" title="BMW Z4 sDrive35i"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">BMW Z4 sDrive35i</a></h3>
+				<p class="product_item_price">3 532 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+
+		<figure class="product_item">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_1.png" alt="BMW X3 2.0d" title="BMW X3 2.0d"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">BMW X3 2.0d</a></h3>
+				<p class="product_item_price dark_grey">2 230 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+		<figure class="product_item">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_2.png" alt="AUDI A6 3.0 TFSI" title="AUDI A6 3.0 TFSI"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">AUDI A6 3.0 TFSI</a></h3>
+				<p class="product_item_price dark_grey">2 870 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+		<figure class="product_item">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_3.png" alt="Mercedes-Benz A200" title="Mercedes-Benz A200"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">Mercedes-Benz A200</a></h3>
+				<p class="product_item_price dark_grey">1 310 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+		<figure class="product_item">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_4.png" alt="BMW Z4 sDrive35i" title="BMW Z4 sDrive35i"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">BMW Z4 sDrive35i</a></h3>
+				<p class="product_item_price">3 532 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+
+		<figure class="product_item">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_1.png" alt="BMW X3 2.0d" title="BMW X3 2.0d"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">BMW X3 2.0d</a></h3>
+				<p class="product_item_price dark_grey">2 230 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+		<figure class="product_item">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_2.png" alt="AUDI A6 3.0 TFSI" title="AUDI A6 3.0 TFSI"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">AUDI A6 3.0 TFSI</a></h3>
+				<p class="product_item_price dark_grey">2 870 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+		<figure class="product_item">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_3.png" alt="Mercedes-Benz A200" title="Mercedes-Benz A200"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">Mercedes-Benz A200</a></h3>
+				<p class="product_item_price dark_grey">1 310 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+		<figure class="product_item">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_4.png" alt="BMW Z4 sDrive35i" title="BMW Z4 sDrive35i"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">BMW Z4 sDrive35i</a></h3>
+				<p class="product_item_price">3 532 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+
+		<figure class="product_item last">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_3.png" alt="Mercedes-Benz A200" title="Mercedes-Benz A200"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">Mercedes-Benz A200</a></h3>
+				<p class="product_item_price dark_grey">1 310 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+		<figure class="product_item last">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_4.png" alt="BMW Z4 sDrive35i" title="BMW Z4 sDrive35i"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">BMW Z4 sDrive35i</a></h3>
+				<p class="product_item_price">3 532 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+		<figure class="product_item last">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_3.png" alt="Mercedes-Benz A200" title="Mercedes-Benz A200"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">Mercedes-Benz A200</a></h3>
+				<p class="product_item_price dark_grey">1 310 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+		<figure class="product_item last">
+			<div class="product_item_pict">
+				<a href="#">
+					<img src="/bitrix/templates/.default/images/test_top_week_4.png" alt="BMW Z4 sDrive35i" title="BMW Z4 sDrive35i"/>
+				</a>
+			</div>
+			<figcaption>
+				<h3><a href="#">BMW Z4 sDrive35i</a></h3>
+				<p class="product_item_price">3 532 000 руб.</p>
+				<a class="buy_button inverse inline-block pie" href="#buy_popup" rel="modal:open">В корзину</a>
+			</figcaption>
+		</figure>
+	</div>
+	<div class="clear"></div>
+	<div class="page_nav">
+		<nav>
+			<a href="#" class="prev"></a>
+			<span class="current">1</span>
+			<a href="#">2</a>
+			<a href="#">3</a>
+			<a href="#">4</a>
+			<a href="#">5</a>
+			<span>...</span>
+			<a href="#">87</a>
+			<a href="#" class="next"></a>
+		</nav>
+	</div>
+					</section>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
