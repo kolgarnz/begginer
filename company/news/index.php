36aa59f8d6f4b66335edcb06de3b2ff8f3685e96
Igor Pinchuk <i.pinchuk@qsoft.ru>
2015-05-27 11:56:21 +0300

    57056 / Разработка компонента «Новости» / компонент news.list кастомизирован в пределах bitrix:news

--- company/news/index.php
+++ company/news/index.php
@@ -1,10 +1,10 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Новости");
-?><?$APPLICATION->IncludeComponent("bitrix:news", ".default", array(
+?><?$APPLICATION->IncludeComponent("bitrix:news", "news_inner", array(
 	"IBLOCK_TYPE" => "news",
 	"IBLOCK_ID" => "24",
-	"NEWS_COUNT" => "20",
+	"NEWS_COUNT" => "10",
 	"USE_SEARCH" => "N",
 	"USE_RSS" => "N",
 	"USE_RATING" => "N",
@@ -23,7 +23,7 @@ $APPLICATION->SetTitle("Новости");
 	"AJAX_OPTION_STYLE" => "Y",
 	"AJAX_OPTION_HISTORY" => "N",
 	"CACHE_TYPE" => "A",
-	"CACHE_TIME" => "36000000",
+	"CACHE_TIME" => "3600",
 	"CACHE_FILTER" => "N",
 	"CACHE_GROUPS" => "Y",
 	"SET_TITLE" => "Y",
@@ -31,7 +31,7 @@ $APPLICATION->SetTitle("Новости");
 	"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
 	"ADD_SECTIONS_CHAIN" => "N",
 	"USE_PERMISSIONS" => "N",
-	"PREVIEW_TRUNCATE_LEN" => "",
+	"PREVIEW_TRUNCATE_LEN" => "150",
 	"LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",
 	"LIST_FIELD_CODE" => array(
 		0 => "",
@@ -63,11 +63,11 @@ $APPLICATION->SetTitle("Новости");
 	"DISPLAY_TOP_PAGER" => "N",
 	"DISPLAY_BOTTOM_PAGER" => "Y",
 	"PAGER_TITLE" => "Новости",
-	"PAGER_SHOW_ALWAYS" => "Y",
-	"PAGER_TEMPLATE" => "",
+	"PAGER_SHOW_ALWAYS" => "N",
+	"PAGER_TEMPLATE" => "news.inner",
 	"PAGER_DESC_NUMBERING" => "N",
 	"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-	"PAGER_SHOW_ALL" => "Y",
+	"PAGER_SHOW_ALL" => "N",
 	"DISPLAY_DATE" => "Y",
 	"DISPLAY_PICTURE" => "Y",
 	"DISPLAY_PREVIEW_TEXT" => "Y",
