e10cb59eba3edf72a521c03901bd52cf92954dd3
Igor Pinchuk <i.pinchuk@qsoft.ru>
2015-06-09 13:03:07 +0300

    Подчищен немного шаблон search.page

--- bitrix/templates/.default/components/bitrix/search.page/clear_ris/template.php
+++ bitrix/templates/.default/components/bitrix/search.page/clear_ris/template.php
@@ -2,8 +2,7 @@
 
 <div class="search-page">
 	<form action="" method="get">
-		<input type="hidden" name="tags" value="<?echo $arResult["REQUEST"]["TAGS"]?>" />
-		<input type="hidden" name="how" value="<?echo $arResult["REQUEST"]["HOW"]=="d"? "d": "r"?>" />
+		<input type="hidden" name="how" value="<?=$arResult["REQUEST"]["HOW"]=="d"? "d": "r"?>" />
 		<table width="100%" border="0" cellpadding="0" cellspacing="0">
 			<tbody><tr>
 				<td style="width: 100%;">
@@ -16,21 +15,20 @@
 							<option value="<?=$key?>"<?if($arResult["REQUEST"]["WHERE"]==$key) echo " selected"?>><?=$value?></option>
 						<?endforeach?>
 				</select>
-
 				</td>
 				<td>
-					<input class="search-button" type="submit" value="<?echo GetMessage("CT_BSP_GO")?>" />
+					<input class="search-button" type="submit" value="<?=GetMessage("CT_BSP_GO")?>" />
 				</td>
 			</tr>
 		</tbody></table>
 	</form>
 
-<?if(isset($arResult["REQUEST"]["ORIGINAL_QUERY"])):
-	?>
-	<div class="search-language-guess">
-		<?echo GetMessage("CT_BSP_KEYBOARD_WARNING", array("#query#"=>'<a href="'.$arResult["ORIGINAL_QUERY_URL"].'">'.$arResult["REQUEST"]["ORIGINAL_QUERY"].'</a>'))?>
-	</div><br />
-<?endif;?>
+    <?if(isset($arResult["REQUEST"]["ORIGINAL_QUERY"])):
+        ?>
+        <div class="search-language-guess">
+            <?=GetMessage("CT_BSP_KEYBOARD_WARNING", array("#query#"=>'<a href="'.$arResult["ORIGINAL_QUERY_URL"].'">'.$arResult["REQUEST"]["ORIGINAL_QUERY"].'</a>'))?>
+        </div><br />
+    <?endif;?>
 
 	<div class="search-result">
 	<?if($arResult["ERROR_CODE"]!=0):?>
@@ -66,31 +64,30 @@
 		<?if($arParams["DISPLAY_TOP_PAGER"] != "N") echo $arResult["NAV_STRING"]?>
 		<?foreach($arResult["SEARCH"] as $arItem):?>
 			<div class="search-item">
-				<h4><a href="<?echo $arItem["URL"]?>"><?echo $arItem["TITLE_FORMATED"]?></a></h4>
-				<div class="search-preview"><?echo $arItem["BODY_FORMATED"]?></div>
+				<h4><a href="<?=$arItem["URL"]?>"><?=$arItem["TITLE_FORMATED"]?></a></h4>
+				<div class="search-preview"><?=$arItem["BODY_FORMATED"]?></div>
 
 				<?if(
 					($arParams["SHOW_ITEM_DATE_CHANGE"] != "N")
 				):?>
 				<div class="search-item-meta">
-					<div class="search-item-date"><label><?echo GetMessage("CT_BSP_DATE_CHANGE")?>: </label><span><?echo $arItem["DATE_CHANGE"]?></span></div>
+					<div class="search-item-date"><label><?=GetMessage("CT_BSP_DATE_CHANGE")?>: </label><span><?=$arItem["DATE_CHANGE"]?></span></div>
 				</div>
 				<?endif?>
 			</div>
 		<?endforeach;?>
 		<?if($arParams["DISPLAY_BOTTOM_PAGER"] != "N") echo $arResult["NAV_STRING"]?>
 		<?if($arParams["SHOW_ORDER_BY"] != "N"):?>
-			<div class="search-sorting"><label><?echo GetMessage("CT_BSP_ORDER")?>:</label>&nbsp;
-			<?if($arResult["REQUEST"]["HOW"]=="d"):?>
-				<a href="<?=$arResult["URL"]?>&amp;how=r"><?=GetMessage("CT_BSP_ORDER_BY_RANK")?></a>&nbsp;<b><?=GetMessage("CT_BSP_ORDER_BY_DATE")?></b>
-			<?else:?>
-				<b><?=GetMessage("CT_BSP_ORDER_BY_RANK")?></b>&nbsp;<a href="<?=$arResult["URL"]?>&amp;how=d"><?=GetMessage("CT_BSP_ORDER_BY_DATE")?></a>
-			<?endif;?>
+			<div class="search-sorting"><label><?=GetMessage("CT_BSP_ORDER")?>:</label>&nbsp;
+                <?if($arResult["REQUEST"]["HOW"]=="d"):?>
+                    <a href="<?=$arResult["URL"]?>&amp;how=r"><?=GetMessage("CT_BSP_ORDER_BY_RANK")?></a>&nbsp;<b><?=GetMessage("CT_BSP_ORDER_BY_DATE")?></b>
+                <?else:?>
+                    <b><?=GetMessage("CT_BSP_ORDER_BY_RANK")?></b>&nbsp;<a href="<?=$arResult["URL"]?>&amp;how=d"><?=GetMessage("CT_BSP_ORDER_BY_DATE")?></a>
+                <?endif;?>
 			</div>
 		<?endif;?>
 	<?else:?>
 		<?ShowNote(GetMessage("CT_BSP_NOTHING_TO_FOUND"));?>
 	<?endif;?>
-
 	</div>
 </div>
