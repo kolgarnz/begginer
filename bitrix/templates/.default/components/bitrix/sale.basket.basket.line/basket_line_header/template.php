f61b21e69880818b39470573c1a9305042077e5e
Igor Pinchuk <i.pinchuk@qsoft.ru>
2015-06-09 21:54:26 +0300

    поправлен sale.basket.basket.line и подчищен

--- bitrix/templates/.default/components/bitrix/sale.basket.basket.line/basket_line_header/template.php
+++ bitrix/templates/.default/components/bitrix/sale.basket.basket.line/basket_line_header/template.php
@@ -2,5 +2,10 @@
 
 <div class="basket_block inline-block">
 	<a href="<?=$arParams["PATH_TO_BASKET"]?>" class="basket_product_count inline-block"><?=$arResult["NUM_PRODUCTS"];?></a>
-	<a <?if (IntVal($arResult["NUM_PRODUCTS"])>0):?>href="<?=$arParams["PATH_TO_BASKET"]?>"<?endif?> class="order_button pie <?if (IntVal($arResult["NUM_PRODUCTS"])>0):?>active<?endif?>"><?=GetMessage('TSB1_MAKE_ORDER')?></a>
+	<a
+        <?=intval($arResult["NUM_PRODUCTS"]) ? $arParams["PATH_TO_BASKET"] : '' ?>
+        class="order_button pie <?=intval($arResult["NUM_PRODUCTS"]) ? 'active' : '' ?>
+    ">
+        <?=GetMessage('TSB1_MAKE_ORDER')?>
+    </a>
 </div>
