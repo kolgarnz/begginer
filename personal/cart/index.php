aa20fb3808212e2ab75f7213c87964489e6c8cd5
Igor Pinchuk <i.pinchuk@qsoft.ru>
2015-06-10 13:56:36 +0300

    small fix

--- personal/cart/index.php
+++ personal/cart/index.php
@@ -1,6 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Корзина");
+$APPLICATION->SetTitle("Моя Корзина");
 ?><?$APPLICATION->IncludeComponent("qsoft:eshop.sale.basket.basket", "basket", array(
 	"COUNT_DISCOUNT_4_ALL_QUANTITY" => "Y",
 	"COLUMNS_LIST" => array(
